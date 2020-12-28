4e070272816a116226eba6625097e29fb22d7fa1
Sklyanskiy <mmm207142@gmail.com>
2020-12-28 19:36:18 +0200

    Added ECS integration for lobby.

---
+++ Match-3/Assets/Scripts/Systems/Lobby/LobbySceneData.cs
@@ -0,0 +1,9 @@
+﻿using Match3.Assets.Scripts.UnityComponents.UI.InGame;
+using UnityEngine;
+
+public class LobbySceneData : MonoBehaviour
+{
+    [SerializeField] private Camera _camera = null;
+
+    public Camera Camera { get => _camera; }
+}
