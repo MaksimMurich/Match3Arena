15d1565b8b084ebebff1a27ebc58339a81cf39cb
Sklyanskiy <mmm207142@gmail.com>
2021-01-19 14:35:03 +0200

    Fixes. Refactoring.

--- Match-3/Assets/Scripts/Systems/Game/Other/ManageTurnTimeSystem.cs
+++ Match-3/Assets/Scripts/Systems/Game/Other/ManageTurnTimeSystem.cs
@@ -3,67 +3,69 @@ using UnityEngine;
 using DG.Tweening;
 using Match3.Components.Game.Events;
 
-namespace Match3.Systems.Game {
-    sealed class ManageTurnTimeSystem : IEcsRunSystem, IEcsInitSystem {
+namespace Match3.Systems.Game
+{
+	sealed class ManageTurnTimeSystem : IEcsRunSystem, IEcsInitSystem
+	{
 		private readonly EcsFilter<SwapRequest> _swapFilter = null;
 		private readonly EcsFilter<PlayerChangedEvent> _playerChanged = null;
 
-        private int _timeViewRemain = 0;
-        private bool _isTimerActive = false;
-        private static readonly int _expirationDelay = Global.Config.InGame.ExpirationDelay;
-        private float _timeRemain = Global.Config.InGame.MaxTurnTime + _expirationDelay;
+		private int _timeViewRemain = 0;
+		private bool _isTimerActive = false;
+		private static readonly int _expirationDelay = Global.Config.InGame.ExpirationDelay;
+		private float _timeRemain = Global.Config.InGame.MaxTurnTime + _expirationDelay;
 
 		public void Init()
 		{
-            Sequence sequence = DOTween.Sequence();
-            sequence.SetDelay(Global.Config.InGame.Animation.SelectFirstPlayerDuration);
-            sequence.OnComplete(() =>
-            {
-                _isTimerActive = true;
-                ResetTimeRemain();
-            });
-        }
+			Sequence sequence = DOTween.Sequence();
+			sequence.SetDelay(Global.Config.InGame.Animation.SelectFirstPlayerDuration);
+			sequence.OnComplete(() =>
+			{
+				_isTimerActive = true;
+				ResetTimeRemain();
+			});
+		}
 
-        void IEcsRunSystem.Run()
-        {
-            _isTimerActive = _isTimerActive && _swapFilter.GetEntitiesCount() == 0;
-            _isTimerActive = _isTimerActive || _playerChanged.GetEntitiesCount() > 0;
+		void IEcsRunSystem.Run()
+		{
+			_isTimerActive = _isTimerActive && _swapFilter.GetEntitiesCount() == 0;
+			_isTimerActive = _isTimerActive || _playerChanged.GetEntitiesCount() > 0;
 
-            bool needResetTimer = _playerChanged.GetEntitiesCount() > 0;
-            if (needResetTimer)
-            {
-                ResetTimeRemain();
-            }
+			bool needResetTimer = _playerChanged.GetEntitiesCount() > 0;
+			if (needResetTimer)
+			{
+				ResetTimeRemain();
+			}
 
-            if (_isTimerActive)
-            {
-                _timeRemain -= Time.deltaTime;
+			if (_isTimerActive)
+			{
+				_timeRemain -= Time.deltaTime;
 
 				bool needUpdateView = _timeViewRemain != (int)(_timeRemain - (float)_expirationDelay);
 				if (needUpdateView)
 				{
-                    UpdateView();
+					UpdateView();
 				}
-            }
+			}
 
-            if (_timeRemain <= 0)
-            {
-                Global.Data.InGame.World.NewEntity().Set<TurnTimeIsUpEvent>();
-                ResetTimeRemain();
-            }
-        }
+			if (_timeRemain <= 0)
+			{
+				Global.Data.InGame.World.NewEntity().Set<TurnTimeIsUpEvent>();
+				ResetTimeRemain();
+			}
+		}
 
-        private void ResetTimeRemain()
-        {
-            _timeRemain = Global.Config.InGame.MaxTurnTime + _expirationDelay + (2*Time.deltaTime); // do not remove additional deltaTime. It will cause visual bug.
-            UpdateView();
-        }
-
-        private void UpdateView()
+		private void ResetTimeRemain()
 		{
-            _timeViewRemain = Mathf.Max((int)(_timeRemain - _expirationDelay), 0);
-            EcsEntity timerUpdateRequest = Global.Data.InGame.World.NewEntity();
-            timerUpdateRequest.Set<UpdateTurnTimerViewRequest>().TimeRamain = _timeViewRemain;
-        }
+			_timeRemain = Global.Config.InGame.MaxTurnTime + _expirationDelay + (2 * Time.deltaTime); // do not remove additional deltaTime. It will cause visual bug.
+			UpdateView();
+		}
+
+		private void UpdateView()
+		{
+			_timeViewRemain = Mathf.Max((int)(_timeRemain - _expirationDelay), 0);
+			EcsEntity timerUpdateRequest = Global.Data.InGame.World.NewEntity();
+			timerUpdateRequest.Set<UpdateTurnTimerViewRequest>().TimeRamain = _timeViewRemain;
+		}
 	}
 }
--- Match-3/Assets/Scripts/Systems/Game/Other/ManageTurnTimeViewSystem.cs
+++ Match-3/Assets/Scripts/Systems/Game/Other/ManageTurnTimeViewSystem.cs
@@ -5,137 +5,135 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
-namespace Match3.Systems.Game {
-    sealed class ManageTurnTimeViewSystem : IEcsRunSystem, IEcsInitSystem {
+namespace Match3.Systems.Game
+{
+	sealed class ManageTurnTimeViewSystem : IEcsRunSystem, IEcsInitSystem
+	{
 
-        private readonly EcsFilter<UpdateTurnTimerViewRequest> _updateTurnTimerRequestsFilter = null;
-        private readonly EcsFilter<NextPlayerRequest> _nextPlayerRequestsfilter = null;
+		private readonly EcsFilter<UpdateTurnTimerViewRequest> _updateTurnTimerRequestsFilter = null;
+		private readonly EcsFilter<NextPlayerRequest> _nextPlayerRequestsfilter = null;
 
-        private readonly string _defaultTimerView = "0";
-        private readonly float _timeToSignal = Global.Config.InGame.TurnTimerSignalTime;
-        private readonly float _scaleCoefficient = Global.Config.InGame.TurnTimerScaleCoefficient;
-        private readonly float _defaultScale = 1f;
-        private readonly float _animationDuration = 0.8f; // do not set more than 1
-        private int _timeRemain = (int)Global.Config.InGame.MaxTurnTime;
-        private bool _lastTickMade = false;
+		private readonly string _defaultTimerView = "0";
+		private readonly float _timeToSignal = Global.Config.InGame.TurnTimerSignalTime;
+		private readonly float _scaleCoefficient = Global.Config.InGame.TurnTimerScaleCoefficient;
+		private readonly float _defaultScale = 1f;
+		private readonly float _animationDuration = 0.8f; // 0 < value < 1
+		private int _timeRemain = (int)Global.Config.InGame.MaxTurnTime;
+		private bool _lastTickMade = false;
 
-        private readonly Color _botViewColorDefault = Global.Views.InGame.BotDataView.TurnTimer.color;
-        private readonly Color _playerViewColorDefault = Global.Views.InGame.PlayerDataView.TurnTimer.color;
-        private readonly Color _singnalColor = Color.red;
+		private readonly Color _botViewColorDefault = Global.Views.InGame.BotDataView.TurnTimer.color;
+		private readonly Color _playerViewColorDefault = Global.Views.InGame.PlayerDataView.TurnTimer.color;
+		private readonly Color _singnalColor = Color.red;
 
-        private Text _botView = Global.Views.InGame.BotDataView.TurnTimer;
-        private Text _playerView = Global.Views.InGame.PlayerDataView.TurnTimer;
+		private Text _botView = Global.Views.InGame.BotDataView.TurnTimer;
+		private Text _playerView = Global.Views.InGame.PlayerDataView.TurnTimer;
 
-        private Sequence _viewsAnimation = DOTween.Sequence();
+		private Sequence _viewsAnimation = DOTween.Sequence();
 
-        public void Init()
+		public void Init()
 		{
 			_botView.text = _defaultTimerView;
 			_playerView.text = _defaultTimerView;
 
-            SetScaleViewsDefault();
-            SetViewsColor();
-            Hide();
-        }
+			SetScaleViewsDefault();
+			SetViewsColor();
+			Hide();
+		}
 
-        public void Run()
-        {
-            DeactivateIfNeed();
+		public void Run()
+		{
+			DeactivateIfNeed();
 
-            bool timeChanged = _updateTurnTimerRequestsFilter.GetEntitiesCount() > 0;
+			bool timeChanged = _updateTurnTimerRequestsFilter.GetEntitiesCount() > 0;
 			if (!timeChanged)
-            {
-                return;
-            }
+			{
+				return;
+			}
 
-            Debug.Log("Timer has been updated...");
+			UpdateTurnTimerViewRequest updateTurnTimerViewRequest = _updateTurnTimerRequestsFilter.Get1(0);
+			_timeRemain = updateTurnTimerViewRequest.TimeRamain;
 
-            UpdateTurnTimerViewRequest updateTurnTimerViewRequest = _updateTurnTimerRequestsFilter.Get1(0);
-            _timeRemain = updateTurnTimerViewRequest.TimeRamain;
-            int timeViewValue = _timeRemain;
-            timeViewValue = Mathf.Max(timeViewValue, 0);
+			_botView.text = _timeRemain.ToString();
+			_playerView.text = _timeRemain.ToString();
 
-            _botView.text = timeViewValue.ToString();
-            _playerView.text = timeViewValue.ToString();
-
-            bool isPlayerTime = Global.Data.InGame.PlayerState.Active;
-            _botView.gameObject.SetActive(!isPlayerTime);
-            _playerView.gameObject.SetActive(isPlayerTime);
+			bool isPlayerTime = Global.Data.InGame.PlayerState.Active;
+			_botView.gameObject.SetActive(!isPlayerTime);
+			_playerView.gameObject.SetActive(isPlayerTime);
 
 			if (_timeRemain <= _timeToSignal)
 			{
 				ScaleViews();
-                SetViewsColor(_singnalColor);
-            }
+				SetViewsColor(_singnalColor);
+			}
 			else
 			{
-                SetScaleViewsDefault();
-                SetViewsColor();
-            }
-		}
-
-        private void DeactivateIfNeed()
-        {
-            if(_nextPlayerRequestsfilter.GetEntitiesCount() > 0)
-            {
-                SetScaleViewsDefault();
-                SetViewsColor();
-                Hide();
-            }
-        }
-
-        private void Hide()
-        {
-            _botView.gameObject.SetActive(false);
-            _playerView.gameObject.SetActive(false);
-        }
-
-        private void ScaleViews()
-		{
-            SetScaleViewsDefault();
-
-            if(_timeRemain == 1)
-			{
-                _lastTickMade = false;
+				SetScaleViewsDefault();
+				SetViewsColor();
 			}
-            else if(_timeRemain <= 0 && _lastTickMade == true)
+		}
+
+		private void DeactivateIfNeed()
+		{
+			if (_nextPlayerRequestsfilter.GetEntitiesCount() > 0)
 			{
-                return;
+				SetScaleViewsDefault();
+				SetViewsColor();
+				Hide();
 			}
-            else if(_timeRemain <= 0)
+		}
+
+		private void Hide()
+		{
+			_botView.gameObject.SetActive(false);
+			_playerView.gameObject.SetActive(false);
+		}
+
+		private void ScaleViews()
+		{
+			SetScaleViewsDefault();
+
+			if (_timeRemain == 1)
 			{
-                _lastTickMade = true;
-            }
+				_lastTickMade = false;
+			}
+			else if (_timeRemain <= 0 && _lastTickMade == true)
+			{
+				return;
+			}
+			else if (_timeRemain <= 0)
+			{
+				_lastTickMade = true;
+			}
 
-            Tween upscalingBot = _botView.transform.DOScale(new Vector3(_scaleCoefficient, _scaleCoefficient), _animationDuration/2);
-            Tween downscalingBot = _botView.transform.DOScale(new Vector3(_defaultScale, _defaultScale), _animationDuration/2);
-            Tween upscalingPlayer = _playerView.transform.DOScale(new Vector3(_scaleCoefficient, _scaleCoefficient), _animationDuration/2);
-            Tween downscalingPlayer = _playerView.transform.DOScale(new Vector3(_defaultScale, _defaultScale), _animationDuration/2);
-            
-            _viewsAnimation.Join(upscalingBot);
-            _viewsAnimation.Join(upscalingPlayer);
-            _viewsAnimation.Append(downscalingBot);
-            _viewsAnimation.Join(downscalingPlayer);
-        }
+			Tween upscalingBot = _botView.transform.DOScale(new Vector3(_scaleCoefficient, _scaleCoefficient), _animationDuration / 2);
+			Tween downscalingBot = _botView.transform.DOScale(new Vector3(_defaultScale, _defaultScale), _animationDuration / 2);
+			Tween upscalingPlayer = _playerView.transform.DOScale(new Vector3(_scaleCoefficient, _scaleCoefficient), _animationDuration / 2);
+			Tween downscalingPlayer = _playerView.transform.DOScale(new Vector3(_defaultScale, _defaultScale), _animationDuration / 2);
 
-        private void SetScaleViewsDefault()
-		{
-            _viewsAnimation.Kill();
-            _viewsAnimation = DOTween.Sequence();
-            _botView.transform.localScale = new Vector3(_defaultScale, _defaultScale);
-            _playerView.transform.localScale = new Vector3(_defaultScale, _defaultScale);
+			_viewsAnimation.Join(upscalingBot);
+			_viewsAnimation.Join(upscalingPlayer);
+			_viewsAnimation.Append(downscalingBot);
+			_viewsAnimation.Join(downscalingPlayer);
 		}
 
-        private void SetViewsColor()
+		private void SetScaleViewsDefault()
 		{
-            _botView.color = _botViewColorDefault;
-            _playerView.color = _playerViewColorDefault;
+			_viewsAnimation.Kill();
+			_viewsAnimation = DOTween.Sequence();
+			_botView.transform.localScale = new Vector3(_defaultScale, _defaultScale);
+			_playerView.transform.localScale = new Vector3(_defaultScale, _defaultScale);
 		}
 
-        private void SetViewsColor(Color color)
+		private void SetViewsColor()
 		{
-            _playerView.color = color;
-            _botView.color = color;
+			_botView.color = _botViewColorDefault;
+			_playerView.color = _playerViewColorDefault;
 		}
-    }
+
+		private void SetViewsColor(Color color)
+		{
+			_playerView.color = color;
+			_botView.color = color;
+		}
+	}
 }
